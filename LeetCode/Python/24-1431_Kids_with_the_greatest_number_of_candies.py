723e6184d2ffefe09a232febc966d890f3772d8a
Abel Tadesse <tadesseabel26@gmail.com>
2024-05-27 09:07:29 +0300

    leetCode_24

---
+++ LeetCode/Python/24-1431_Kids_with_the_greatest_number_of_candies.py
@@ -0,0 +1,9 @@
+class Solution(object):
+    def kidsWithCandies(self, candies, extraCandies):
+        """
+        :type candies: List[int]
+        :type extraCandies: int
+        :rtype: List[bool]
+        """
+        return list(map(lambda candy: candy + extraCandies >= max(candies), candies))
+        
