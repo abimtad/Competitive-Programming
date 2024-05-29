e80e6727b9ae725ea57b4aa6fb46e97f5bc3e35a
Abel Tadesse <tadesseabel26@gmail.com>
2024-05-29 11:25:27 +0300

    leetCode_29

---
+++ LeetCode/Python/29-2810_Faulty_keyboard.py
@@ -0,0 +1,16 @@
+class Solution(object):
+    def finalString(self, s):
+        """
+        :type s: str
+        :rtype: str
+        """
+        ans = ""
+
+        for char in s:
+            if char == 'i':
+                ans = ans[::-1]
+            else:
+                ans += char
+
+        return ans
+
