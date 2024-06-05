b12631f0731470e15e3cf1f92bde6e1ef62e2c65
Abel Tadesse <tadesseabel26@gmail.com>
2024-06-06 00:19:08 +0300

    leetCode_37

---
+++ LeetCode/Python/37-1832_Check_if_the_sentance_is_panagram.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def checkIfPangram(self, sentence):
+        """
+        :type sentence: str
+        :rtype: bool
+        """
+        unique_letters = set()
+
+        for char in sentence:
+            unique_letters.add(char)
+            
+        return len(unique_letters) == 26
+    
